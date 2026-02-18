cc3ac6eabb2145c8b1a52100ae01d58fbd0d2e0c
yashvant11 <prajapatiyashvant13@gmail.com>
2026-02-18 13:39:04 +0530

    siderbar menu add by yash, candidate lish UI and api done by yash

--- masterapplication.client/src/layout/layout.routes.ts
+++ masterapplication.client/src/layout/layout.routes.ts
@@ -20,6 +20,16 @@ export const routes: Routes = [
         path: 'registration',
         loadComponent: () => import('./user-registration-details/registration-add/registration-add.component').then(m => m.RegistrationAddComponent),
       },
+        {
+        path: 'candidateList',
+        loadComponent: () => import('./user-registration-details/registration-list/registration-list.component').then(m => m.RegistrationListComponent),
+      },
+      {
+      path: 'under-construction',
+      loadComponent: () => import('../shared-library/component/under-construction/under-construction.component').then(m => m.UnderConstructionComponent),
+    
+      }
+
     ]
   }
 ]
--- masterapplication.client/src/layout/user-registration-details/registration-list/registration-list.component.ts
+++ masterapplication.client/src/layout/user-registration-details/registration-list/registration-list.component.ts
@@ -1,11 +1,126 @@
 import { Component } from '@angular/core';
+import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { firstValueFrom } from 'rxjs';
+import { SharedLibraryModule } from '../../../shared-library/shared-library.module';
+import { MasterTableComponent } from '../../../shared-library/component/master-table/Master-table.component';
+import { TablePaginationSettingsConfig } from '../../../shared-library/component/master-table/table-settings.model';
+import { ApiService } from '../../../shared-library/service/api.service';
+import { AuthService } from '../../../shared-library/service/auth.service';
+import { PersonalInfo } from '../../../shared-library/models/personalinfo.model';
+
+
 
 @Component({
-  selector: 'app-registration-list',
-  imports: [],
-  templateUrl: './registration-list.component.html',
-  styleUrl: './registration-list.component.css',
+    selector: 'app-registration-list',
+    imports: [SharedLibraryModule, MasterTableComponent, NgxSpinnerModule],
+    templateUrl: './registration-list.component.html',
+    styleUrl: './registration-list.component.css'
 })
-export class RegistrationListComponent {
 
+export class RegistrationListComponent extends TablePaginationSettingsConfig {
+  personalInfo: PersonalInfo[] = [];
+  constructor(private spinner: NgxSpinnerService,private toastr:ToastrService,private apiService: ApiService,private authService: AuthService) {
+    super();
+    this.tablePaginationSettings.enableView=true;
+    this.tablePaginationSettings.enableColumn=true;
+    this.tablePaginationSettings.enableAction=true;
+    this.tablePaginationSettings.enableColumn = true;
+    this.tablePaginationSettings.pageSizeOptions = [50, 100];
+    this.tablePaginationSettings.showFirstLastButtons = false;
+    this.tablePaginationSettings.enableEdit = true;
+    this.tablePaginationSettings.enableDelete = true;
+    
+    
+  }
+ ngOnInit(){
+
+    this.getList();
+  }
+  getList() {
+    this.spinner.show();
+    this.apiService.getWithHeaders('registration/personalInfo').subscribe(res => {
+        if (res) {
+          this.personalInfo = res;
+          this.spinner.hide();
+        }
+    });
+  }
+  view(){
+
+  }
+edit(){
+
+}
+del(){
+
+}
+
+
+  columns = [
+
+    {
+      name: 'name',
+      displayName: 'Full Name',
+      isSearchable: true,
+      hide: false,
+      type: 'text',
+    },
+    {
+      name: 'unit',
+      displayName: 'Unit',
+      isSearchable: true,
+      hide: false,
+      type: 'text',
+    },
+    {
+      name: 'parentArm',
+      displayName: 'Parent Arm',
+      isSearchable: true,
+      hide: false,
+      type: 'text',
+    },
+    {
+      name: 'dtOfCommission',
+      displayName: 'Dt Of Commission',
+      isSearchable: true,
+      hide: false,
+      type: 'text',
+    },
+    {
+      name: 'dtOfSeniority',
+      displayName: 'Dt Of Seniority',
+      isSearchable: true,
+      hide: false,
+      type: 'text',
+    },
+    {
+      name: 'awardOfWings',
+      displayName: 'Award Of Wings',
+      isSearchable: true,
+      hide: false,
+      type: 'text',
+    },
+    {
+      name: 'apptHeld',
+      displayName: 'Appt Held',
+      isSearchable: true,
+      hide: true,
+      type: 'text',
+    },
+    {
+      name: 'medCategory',
+      displayName: 'Med Cat',
+      isSearchable: true,
+      hide: true,
+      type: 'text',
+    },
+    {
+      name: 'instrCategory',
+      displayName: 'Instr Cat',
+      isSearchable: true,
+      hide: true,
+      type: 'text',
+    },
+  ];
 }
